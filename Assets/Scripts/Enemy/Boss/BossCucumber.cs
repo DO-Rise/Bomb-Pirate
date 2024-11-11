03e0f20653e441c409e4eba49b7b0b53c4071a64
Rise <fanworkrise@gmail.com>
2024-11-11 12:05:29 +0200

    Add all boss script (empty)

---
+++ Assets/Scripts/Enemy/Boss/BossCucumber.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCucumber : Boss
+{
+    protected override void FirstStage()
+    {
+
+    }
+
+    protected override void SecondStage()
+    {
+
+    }
+
+    protected override void ThirdStage()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Enemy/Boss/BossWhale.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossWhale : Boss
+{
+    protected override void FirstStage()
+    {
+
+    }
+
+    protected override void SecondStage()
+    {
+
+    }
+
+    protected override void ThirdStage()
+    {
+
+    }
+}
